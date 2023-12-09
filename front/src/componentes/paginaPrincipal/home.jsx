82e25dcfda3d76e003b4760b471244b41e394e59
Diego <Diegofernandomartinezcalderon@gmail.com>
2023-12-09 16:33:49 -0500

    feat: :sparkles: componente Home
    este componente contendrá todos los componentes hijos como el heade, body etc

---
+++ front/src/componentes/paginaPrincipal/home.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import Header from "../paginaPrincipal/header.jsx";
+import Body from "./body";
+
+const Home = () => {
+  return (
+    <>
+    <Header />
+    <Body />
+    </>
+  );
+};
+
+export default Home;
