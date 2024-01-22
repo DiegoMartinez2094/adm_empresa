f5c7f3e977594c4bb3eb3f2f6c866bdbffbe3830
Diego <Diegofernandomartinezcalderon@gmail.com>
2024-01-21 20:15:41 -0500

    feat: :sparkles: verificacion de correo antes del registro
    se verifica en el backend en la base de datos

--- back/routes/usuarios.js
+++ back/routes/usuarios.js
@@ -4,6 +4,37 @@ import { limitConsult } from "../middleware/limite_consulta.js";
 
 const appUsuarios = Router();
 
+appUsuarios.post("/verificarEmail", async (req, res) => {
+  try {
+    const { email_usuario } = req.body;
+
+    // Consultar si el correo electrónico ya está registrado en la tabla usuarios
+    const query = "SELECT * FROM usuarios WHERE email_usuario = ?";
+    conexion.query(query, [email_usuario], (error, results) => {
+      if (error) {
+        console.error("Error al ejecutar la consulta:", error);
+        res
+          .status(500)
+          .json({ message: "Error al verificar correo electrónico" });
+      } else {
+        if (results.length > 0) {
+          // El correo electrónico ya está registrado
+          res.status(200).json({ message: "Correo electrónico ya registrado" });
+          console.log("ya existe el correo electrónico")
+        } else {
+          // El correo electrónico no está registrado
+          res.status(404).json({ message: "Correo electrónico no registrado" });
+          console.log("Correo electrónico no registrado")
+        }
+      }
+    });
+  } catch (error) {
+    // Manejar errores, si los hay
+    console.error("Error al verificar correo electrónico:", error);
+    res.status(500).json({ message: "Error al verificar correo electrónico" });
+  }
+});
+
 appUsuarios.post("/", limitConsult(), (req, res) => {
   conexion.query(`INSERT INTO usuarios SET ?`, req.body, (err, data, fils) => {
     if (err) {
@@ -65,10 +96,9 @@ appUsuarios.get("/PorCedula", limitConsult(), (req, res) => {
 appUsuarios.post("/ingreso", limitConsult(), (req, res) => {
   const email = req.body.email_usuario;
   const contraseña = req.body.contraseña_usuario;
-  if(!email  || !contraseña) {
-    res.status(400).json({message:"informacion invalida"})
-  }
-  else {
+  if (!email || !contraseña) {
+    res.status(400).json({ message: "informacion invalida" });
+  } else {
     conexion.query(
       /*sql*/ `SELECT 
    email_usuario,
@@ -82,11 +112,13 @@ appUsuarios.post("/ingreso", limitConsult(), (req, res) => {
         } else if (!data.length) {
           res.status(404).json({ message: "Usuario no encontrado" });
         } else {
-          res.status(200).json({ message: "Usuario autenticado correctamente" });
+          res
+            .status(200)
+            .json({ message: "Usuario autenticado correctamente" });
         }
       }
     );
-  } 
+  }
 });
 
 appUsuarios.delete("/PorCedula", limitConsult(), (req, res) => {
--- front/src/componentes/crearCuentaLogin/crearCuenta.jsx
+++ front/src/componentes/crearCuentaLogin/crearCuenta.jsx
@@ -3,91 +3,162 @@ import { Link } from "react-router-dom";
 import admLogo from "../../assets/admLogo.png";
 
 const CrearCuenta = () => {
-    return (
-        <>
-          <div class="container">
-            <div class="row">
-              <div class="col-sm">
-                <img className="admLogo2" src={admLogo} />
-              </div>
-              <div class="col-sm">
-                <form className="formLogin">
-                  <div class="mb-3">
-                    <h3>Crear una cuenta</h3>
-                  </div>
-                  <div class="mb-3">
-                    <input
-                      type="text"
-                      class="form-control"
-                      placeholder="Cédula"
-                    />
-                  </div>
-                  <div class="mb-3">
-                    <input
-                      type="text"
-                      class="form-control"
-                      placeholder="Nombre"
-                      name="nombre_usuario"
-                    />
-                  </div>
-                  <div class="mb-3">
-                    <input
-                      type="Email"
-                      class="form-control"
-                      id="exampleInputEmail1"
-                      aria-describedby="emailHelp"
-                      placeholder="Correo Electrónico"
-                      name="email_usuario"
-                    
-                    //   value={email_usuario}
-                    />
-                  </div>
-                  <div class="mb-3">
-                    <input
-                      type="password"
-                      class="form-control"
-                      id="exampleInputPassword1"
-                      placeholder="Contraseña"
-                      name="contraseña_usuario"
-                    //   onChange={onContraseña_empleadoChange}
-                    //   value={contraseña_usuario}
-                    />
-                    <div id="mensaje-error"></div>
-                  </div>
-                  <div class="mb-3">
-                    <input
-                      type="text"
-                      class="form-control"
-                      id="exampleInputEmail1"
-                      placeholder="Telefono"
-                    />
-                  </div>
-                  <button  className="btn-primary">
-                    Crear Cuenta
-                  </button>
-                  <div class="mb-3"> 
-                  <Link to={"/login"} >
-                      <a>Inicio sesión</a>
-                    </Link>
-                    
-                  </div>
-                  <div class="mb-3">
-                    <a>¿Necesitas ayuda? </a>{" "}
-                    <a href="" className="soporteContacto">
-                      Soporte de contacto
-                    </a>
-                  </div>
-                  <div class="mb-3">
-                    <Link to={"/"} className="linkPagPrincipal">
-                      <a>Página principal</a>
-                    </Link>
-                  </div>
-                </form>
-              </div>
-            </div>
-          </div>
-        </>
-      );
+  const [cedula_usuario, setcedula_usuario] = useState("");
+  const [nombre_usuario, setnombre_usuario] = useState("");
+  const [email_usuario, setemail_usuario] = useState("");
+  const [contraseña_usuario, setcontraseña_usuario] = useState("");
+  const [telefono_usuario, settelefono_usuario] = useState("");
+
+  const oncedula_usuarioChange = (e) => {
+    setcedula_usuario(e.target.value);
   };
-  
-  export default CrearCuenta;
+  const onnombre_usuarioChange = (e) => {
+    setnombre_usuario(e.target.value);
+  };
+  const onemail_usuarioChange = (e) => {
+    setemail_usuario(e.target.value);
+  };
+  const oncontraseña_usuarioChange = (e) => {
+    setcontraseña_usuario(e.target.value);
+  };
+  const ontelefono_usuarioChange = (e) => {
+    settelefono_usuario(e.target.value);
+  };
+
+  const onRegistroClick = async () => {
+    if (
+      !cedula_usuario ||
+      !nombre_usuario ||
+      !email_usuario ||
+      !contraseña_usuario ||
+      !telefono_usuario
+    ) {
+      alert("todos los campos son obligatorios");
+      return;
+    }
+
+    if (
+      email_usuario.indexOf("@") === -1 ||
+      email_usuario.indexOf(".") === -1
+    ) {
+      // Validar que el campo de correo electrónico contenga al menos una "@" y al menos un "."
+
+      alert("ingrese un correo valido");
+      return;
+    }
+
+    try {
+      // Verificar si el correo electrónico ya está registrado
+      const emailExistsResponse = await fetch(
+        `http://127.0.0.1:3535/usuarios/verificarEmail`,
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ email_usuario }),
+        }
+      );
+
+      if (emailExistsResponse.status === 200) {
+        // El correo electrónico ya está registrado
+        console.log("correo ya registrado");
+        alert("correo electronico ya fue usado anteriormente");
+      }
+    } catch (error) {
+      console.error("Error al realizar la solicitud:", error);
+    }
+  };
+  return (
+    <>
+      <div class="container">
+        <div class="row">
+          <div class="col-sm">
+            <img className="admLogo2" src={admLogo} />
+          </div>
+          <div class="col-sm">
+            <form className="formLogin">
+              <div class="mb-3">
+                <h3>Crear una cuenta</h3>
+              </div>
+              <div class="mb-3">
+                <input
+                  type="text"
+                  class="form-control"
+                  placeholder="Cédula"
+                  onChange={oncedula_usuarioChange}
+                  value={cedula_usuario}
+                />
+              </div>
+              <div class="mb-3">
+                <input
+                  type="text"
+                  class="form-control"
+                  placeholder="Nombre"
+                  onChange={onnombre_usuarioChange}
+                  value={nombre_usuario}
+                />
+              </div>
+              <div class="mb-3">
+                <input
+                  type="Email"
+                  class="form-control"
+                  aria-describedby="emailHelp"
+                  placeholder="Correo Electrónico"
+                  name="email_usuario"
+                  value={email_usuario}
+                  onChange={onemail_usuarioChange}
+
+                  //   value={email_usuario}
+                />
+              </div>
+              <div class="mb-3">
+                <input
+                  type="text"
+                  class="form-control"
+                  id="exampleInputPassword1"
+                  placeholder="Contraseña"
+                  name="contraseña_usuario"
+                  value={contraseña_usuario}
+                  onChange={oncontraseña_usuarioChange}
+                />
+                <div id="mensaje-error"></div>
+              </div>
+              <div class="mb-3">
+                <input
+                  type="text"
+                  class="form-control"
+                  id="exampleInputEmail1"
+                  placeholder="Telefono"
+                  value={telefono_usuario}
+                  onChange={ontelefono_usuarioChange}
+                />
+              </div>
+              <button className="btn-primary" onClick={onRegistroClick}>
+                Crear Cuenta
+              </button>
+              <div class="mb-3">
+                <Link to={"/login"}>
+                  <a>Inicio sesión</a>
+                </Link>
+              </div>
+              <div class="mb-3">
+                <a>¿Necesitas ayuda? </a>{" "}
+                <a href="" className="soporteContacto">
+                  Soporte de contacto
+                </a>
+              </div>
+              <div class="mb-3">
+                <Link to={"/"} className="linkPagPrincipal">
+                  <a>Página principal</a>
+                </Link>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default CrearCuenta;
